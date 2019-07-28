035637ff1b0da37d40fe2df4447ff8be57882732
Wen Sun <syl1887415157@126.com>
2019-07-28 17:47:09 +0800

    feat: add ember plugins(& challenge 1)

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,9 +2,16 @@
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
+
+  const bootstrapPath = 'bower_components/bootstrap-sass/assets/';
+
   let app = new EmberApp(defaults, {
-    // Add options here
+    sassOptions: {
+      includePaths: [
+        bootstrapPath + 'stylesheets'
+      ]
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -19,6 +26,8 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import(bootstrapPath + 'javascripts/bootstrap/collapse.js');
+  app.import(bootstrapPath + 'javascripts/bootstrap/transition.js');
 
   return app.toTree();
 };
