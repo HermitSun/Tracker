7cf5d7ff5f553648561cf32c69b7da56c18d7ba9
Wen Sun <syl1887415157@126.com>
2019-07-28 19:56:31 +0800

    feat: modify routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  beforeModel() {
+    this.transitionTo('sightings');
+  },
+  setupController(controller, model) {
+    this._super(controller, model);
+  }
 });
